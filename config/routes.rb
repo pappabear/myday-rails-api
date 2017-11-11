25869257ba6601dfb6c5767a4c767a43d7a8c9a8
Chip Irek <chip.irek@gmail.com>
2017-11-11 07:21:07 -0500

    Added authentication controller and users controller for signing up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
+  post 'auth/login', to: 'authentication#authenticate'
+  post 'signup', to: 'users#create'
+  
   put '/todos/clear_complete',   to: 'todos#clear_complete', as: 'clear_complete'
   get '/today',                  to: 'todos#today',          as: 'today'
   get '/tomorrow',               to: 'todos#tomorrow',       as: 'tomorrow'
